79af9c9aca195e07e0ad8e1a8c2af5d4f40d3f57
timmy burrito <timothylalbrecht@gmail.com>
2018-10-02 22:29:32 -0400

    added js comments and removed duplicate code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,15 +14,15 @@ var yellowCrystal = Math.floor(Math.random() * (12 - 1) + 1);
 // ---------------------------------------------------------------------------
 
 function resetGame() {
-    numToGet = Math.floor(Math.random() * (120 - 19) + 19);
+    numToGet = Math.floor(Math.random() * (120 - 19) + 19);         // get a random nuumber between 20-119
     console.log(numToGet);
 
-    blueCrystal = Math.floor(Math.random() * (12 - 1) + 1);         
+    blueCrystal = Math.floor(Math.random() * (12 - 1) + 1);         // generates a random number between 1-11 for each crystal
     greenCrystal = Math.floor(Math.random() * (12 - 1) + 1);
     redCrystal = Math.floor(Math.random() * (12 - 1) + 1);
     yellowCrystal = Math.floor(Math.random() * (12 - 1) + 1);
 
-    totalScore = 0;
+    totalScore = 0;                                                 // game starts with 0 points
 
     $("#goal-number").text(numToGet);                               // display a new goal number
     $("#total-score").text(totalScore);                             // reset to 0
@@ -30,7 +30,7 @@ function resetGame() {
     startGame();
 }
 
-function startGame(){
+function startGame(){                                               // reset and start both reference each other
     numToGet = Math.floor(Math.random() * (120 - 19) + 19);
     console.log(numToGet);
 
@@ -44,37 +44,26 @@ function startGame(){
     $("#goal-number").text(numToGet);                               // display a new goal number
     $("#total-score").text(totalScore);                             // reset to 0
 
-    $('.blue').attr("value", blueCrystal);
-    var test = $(".blue").attr("value");
-    //Test / Debug
-    console.log("blue crystal " + test);
-    
+    $('.blue').attr("value", blueCrystal);                          // for each crystal, assigns an attribute name of 'value' and an actual value of ______crystal
+    //var blueNum = $(".blue").attr("value");
+    //console.log("blue crystal " + blueNum);                       // test to make sure the random numbers work
     $('.green').attr("value", greenCrystal);
-    var test = $(".green").attr("value");
-    console.log("green crystal " + test);
-
     $('.red').attr("value", redCrystal);
-    var test = $(".red").attr("value");
-    console.log("red crystal " + test);
-    
     $('.yellow').attr("value", yellowCrystal);
-    var test = $(".yellow").attr("value");
-    console.log("yellow crystal " + test);
     
-    $(".crystal").off().on("click", function()
+    $(".crystal").off().on("click", function()                      // references the crystal class on html. When clicked....
 		{
-			var score = $(this).attr("value");
-			//Adds to final score when clicked
-			totalScore = parseInt(totalScore) + parseInt(score);
+			var score = $(this).attr("value");                      // assign the random value to each crystal
+			totalScore = parseInt(totalScore) + parseInt(score);    // change values to numbers as .attr starts as a string
 			console.log("Crystal Value " +  score);
 			console.log("totalScore is now " + totalScore);
-			$("#total-score").text(totalScore);
+			$("#total-score").text(totalScore);                     // print the new total score
 
 			if(numToGet === totalScore)
 			{
 				alert("Winner!");
 				winTotal++;
-				$("#wins").text(winTotal);
+				$("#wins").text(winTotal);                          // add 1 to wins and print to page
 				$("#total-score").text(0);
 				resetGame();
 				
@@ -83,7 +72,7 @@ function startGame(){
 			{
 				alert("Game Over!");
 				loseTotal++;
-				$("#losses").text(loseTotal);
+				$("#losses").text(loseTotal);                       // add 1 to loss and print to page
 				$("#total-score").text(0);
 				resetGame();
 			}
